207bd4283114934d4a320da17896b5c31fe538a9
Yulya Vorontsova <yulyavorontsova@192.168.1.11>
2021-11-07 14:56:01 +0300

    feat: Add all ciphering functions

--- ciphering.js
+++ ciphering.js
@@ -1,25 +1,40 @@
 const fs = require("fs");
 const path = require("path");
 const {parseCondition} = require("./parseCmdCondition");
+const {caesarCipher} = require("./caesarCipher");
+const {atbashCipher} = require("./atbashCipher");
+const {rotCipher} = require("./rotCipher");
 const { stdin, stdout, exit, argv } = process;
-
-filePath = path.join(__dirname, "", "out.txt");
-
-fs.writeFile(filePath, "Hello", (err) => {
-  if (err) {
-    throw err;
-  }
-});
-
-/*stdin.on("data", function (data) {
-  stdout.write("Hello, ");
-  stdout.write(data);
-  exit();
-});*/
 
 const config = parseCondition(argv, '-c');
 const input = parseCondition(argv, '-i');
 const output = parseCondition(argv, '-o');
-console.log(config)
-console.log(input)
-console.log(output)
+
+fs.readFile(
+    path.join(__dirname, input),
+    'utf-8',
+    (err, data) => {
+      if (err) throw err;
+        let chipherData = data;
+        config.split('-').map((el) => {
+            if (el === 'C1') {
+                chipherData = caesarCipher(chipherData)
+            } else if (el === 'C0') {
+                chipherData = caesarCipher(chipherData, true)
+            }        else    if (el === 'R1') {
+                chipherData = rotCipher(chipherData)
+            } else if (el === 'R0') {
+                chipherData = rotCipher(chipherData, true)
+            }  else if (el === 'A') {
+                chipherData = atbashCipher(chipherData, true)
+            }
+        })
+        fs.writeFile(output, chipherData, (err) => {
+            if (err) {
+                throw err;
+            }
+        });
+    }
+);
+
+
